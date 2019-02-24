bed3e063c7850fd1ca4d46b75a0cd503f2df5d30
chetan253 <cpatil0@outlook.com>
2019-02-24 13:55:52 -0500

    Problem 739

---
+++ leetcode/DailyTemperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int ans[] = new int[T.length];
+        for(int i = 0; i < T.length; i++){
+            for(int j = i+1; j < T.length; j++){
+                if(T[j] > T[i]){
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
