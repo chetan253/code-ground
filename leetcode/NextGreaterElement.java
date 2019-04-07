a6d5b5aa996f929b2260e2b9d0ba0e035c74edd1
chetan253 <cpatil0@outlook.com>
2019-04-06 23:18:36 -0400

    Problem 496

---
+++ leetcode/NextGreaterElement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> s = new Stack<Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i < nums2.length; i++){
+            while(!s.isEmpty() && s.peek() < nums2[i]){
+                map.put(s.pop(), nums2[i]);
+            }
+            s.push(nums2[i]);
+        }
+        
+        for(int i = 0; i < nums1.length; i++){
+            nums1[i] =  map.getOrDefault(nums1[i], -1);
+        }
+        
+        return nums1;
+    }
+}
