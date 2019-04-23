0e08f87ab1623699f51b53465949dc12e80ffc74
chetan253 <cpatil0@outlook.com>
2019-04-23 17:00:42 -0400

    Problem 987

---
+++ leetcode/VerticalOrderTraversalofaBinaryTree.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    public class ANode implements Comparable<ANode>{
+        int val;
+        int x;
+        int y;
+        
+        public ANode(int val, int x, int y){
+            this.val = val;
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public int compareTo(ANode b){
+            if(this.x != b.x){
+                return Integer.compare(this.x, b.x);
+            }
+            else if(this.y != b.y){
+                return Integer.compare(this.y, b.y);
+            }
+            else{
+                return Integer.compare(this.val, b.val);
+            }
+        }
+    }
+    
+    public void dfs(TreeNode node, int x, int y, List<ANode> list){
+        if(node != null){
+            list.add(new ANode(node.val, x, y));            
+            dfs(node.left, x-1, y+1, list);
+            dfs(node.right, x+1, y+1, list);
+        }
+    }
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList();
+        if(root ==null){
+            return ans;
+        }
+        List<ANode> list = new ArrayList<ANode>();
+        dfs(root, 0, 0, list);
+        
+        Collections.sort(list);
+        ans.add(new ArrayList<Integer>());
+        int prevX = list.get(0).x;
+        for(ANode n : list){
+            if(n.x != prevX){
+                ans.add(new ArrayList<Integer>());
+                prevX = n.x;
+            }
+            
+            ans.get(ans.size()-1).add(n.val);
+        }
+        
+        return ans;
+    }
+}
